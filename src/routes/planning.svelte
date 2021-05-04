551611902ff2e321cb3242de10efaa13a439b87b
fabezio <fabezio@outlook..fr>
2021-05-04 16:18:54 +0200

    affichage planning mai

---
+++ src/routes/planning.svelte
@@ -0,0 +1,14 @@
+<script>
+    import plannings from "../data/plannings.js"
+    const {mai} = plannings
+    const {ADS} = mai
+    console.log(ADS)
+
+</script>
+
+<h1>plannings</h1>
+{#each ADS as {jour, agentJour, agentNuit, chefJour, chefNuit}}
+<p>{jour} {chefJour.nom} {chefNuit.nom} {agentJour.nom} {agentNuit.nom}</p>
+{/each}
+<!-- {@debug plannings} -->
+<!-- {plannings.length} -->
