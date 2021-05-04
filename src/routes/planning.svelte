e173e227a2bb3c0c8d4d441001d377c7bd1123c9
fabezio <fabezio@outlook.fr>
2021-05-04 16:36:31 +0200

    organisation cartes

--- src/routes/planning.svelte
+++ src/routes/planning.svelte
@@ -1,14 +1,49 @@
 <script>
-    import plannings from "../data/plannings.js"
-    const {mai} = plannings
-    const {ADS} = mai
-    console.log(ADS)
-
+    import plannings from "../data/plannings.js";
+    const { mai } = plannings;
+    const { ADS } = mai;
+    console.log(ADS);
 </script>
 
 <h1>plannings</h1>
-{#each ADS as {jour, agentJour, agentNuit, chefJour, chefNuit}}
-<p>{jour} {chefJour.nom} {chefNuit.nom} {agentJour.nom} {agentNuit.nom}</p>
+{#each ADS as { jour, agentJour, agentNuit, chefJour, chefNuit }}
+    <div class="flex">
+        <div class="col">
+            {jour}
+        </div>
+        <div class="col">
+            <div class="bold">
+                {chefJour.nom}
+            </div>
+            <div>
+                {agentJour.nom}
+            </div>
+        </div>
+        <div class="col">
+            <div class="bold">
+                {chefNuit.nom}
+            </div>
+            <div>
+                {agentNuit.nom}
+            </div>
+        </div>
+    </div>
 {/each}
 <!-- {@debug plannings} -->
+
 <!-- {plannings.length} -->
+<style>
+    .flex {
+        display: flex;
+        padding: 1rem;
+    }
+    .col {
+        padding: 1rem;
+        display: flex;
+        justify-content: flex-start;
+        flex-direction: column;
+    }
+    .bold {
+        font-weight: bold;
+    }
+</style>
