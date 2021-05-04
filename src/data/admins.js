34a432a7e0f3610ce1ed229c6cdde3ccdcbef9ed
fabezio <fabezio@outlook.fr>
2021-05-04 15:48:02 +0200

    mise en place

---
+++ src/data/admins.js
@@ -0,0 +1,6 @@
+const admins = [
+  { username: 'fabezio', email: 'fabezio@outlook.fr', password: 'Marajade1' },
+  { username: 'dupont', email: 'kikifourien@machin.fr', password: 'trouduc' }
+]
+
+export default admins
---
+++ src/data/plannings.js
@@ -0,0 +1,335 @@
+const chaouti = { nom: 'Chaouti', prenom: 'Brahim' }
+const coulibaly = { nom: 'Coulibaly', prenom: 'Oumar' }
+const dossou = { nom: 'Dossou', prenom: 'Victorin' }
+const kalle = { nom: 'Kalle', prenom: 'Oumar' }
+const lhomme = { nom: 'Lhomme', prenom: 'Pascal' }
+const renaud = { nom: 'Renaud', prenom: 'Cyril' }
+const riquet = { nom: 'Riquet', prenom: 'Fabrice' }
+const bonnet = { nom: 'Bonnet', prenom: 'Sébastien' }
+const disci = { nom: 'Disci', prenom: 'Alex' }
+const frahi = { nom: 'Frahi', prenom: 'Djamal' }
+const jean = { nom: 'Jean', prenom: 'Willy' }
+const ketir = { nom: 'Ketir', prenom: 'Djamel' }
+const khan = { nom: 'Khan', prenom: 'Usman' }
+const koite = { nom: 'Koite', prenom: 'Cueikh Makhfou' }
+const kone = { nom: 'Kone', prenom: 'Oumar' }
+const kuhosa = { nom: 'Kuhosa Gigi', prenom: 'Germain' }
+const vincent = { nom: 'Vincent', prenom: 'Olivier' }
+const bocum = { nom: 'Bocum', prenom: 'Hamidou' }
+const perm = { nom: 'permanence', prenom: '' }
+
+const plannings = {
+  mai: {
+    days: '31',
+    ADS: [
+      {
+        jour: 1,
+        chefJour: coulibaly,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 2,
+        chefJour: coulibaly,
+        chefNuit: dossou,
+        agentJour: disci,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 3,
+        chefJour: renaud,
+        chefNuit: perm,
+        agentJour: bonnet,
+        agentNuit: jean
+      },
+      {
+        jour: 4,
+        chefJour: lhomme,
+        chefNuit: perm,
+        agentJour: bonnet,
+        agentNuit: jean
+      },
+      {
+        jour: 5,
+        chefJour: lhomme,
+        chefNuit: riquet,
+        agentJour: kone,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 6,
+        chefJour: renaud,
+        chefNuit: riquet,
+        agentJour: kone,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 7,
+        chefJour: renaud,
+        chefNuit: bocum,
+        agentJour: kone,
+        agentNuit: koite
+      },
+      {
+        jour: 8,
+        chefJour: lhomme,
+        chefNuit: bocum,
+        agentJour: bonnet,
+        agentNuit: koite
+      },
+      {
+        jour: 9,
+        chefJour: lhomme,
+        chefNuit: riquet,
+        agentJour: bonnet,
+        agentNuit: jean
+      },
+      {
+        jour: 10,
+        chefJour: lhomme,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: jean
+      },
+      {
+        jour: 11,
+        chefJour: lhomme,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 12,
+        chefJour: lhomme,
+        chefNuit: bocum,
+        agentJour: frahi,
+        agentNuit: kuhosa
+      },
+      {
+        jour: 13,
+        chefJour: renaud,
+        chefNuit: bocum,
+        agentJour: frahi,
+        agentNuit: jean
+      },
+      {
+        jour: 14,
+        chefJour: renaud,
+        chefNuit: bocum,
+        agentJour: frahi,
+        agentNuit: dossou
+      },
+      {
+        jour: 15,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: koite
+      },
+      {
+        jour: 16,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: koite
+      },
+      {
+        jour: 17,
+        chefJour: coulibaly,
+        chefNuit: bocum,
+        agentJour: ketir,
+        agentNuit: jean
+      },
+      {
+        jour: 18,
+        chefJour: coulibaly,
+        chefNuit: bocum,
+        agentJour: ketir,
+        agentNuit: jean
+      },
+      {
+        jour: 19,
+        chefJour: chaouti,
+        chefNuit: bocum,
+        agentJour: frahi,
+        agentNuit: khan
+      },
+      {
+        jour: 20,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: frahi,
+        agentNuit: koite
+      },
+      {
+        jour: 21,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: frahi,
+        agentNuit: koite
+      },
+      {
+        jour: 22,
+        chefJour: coulibaly,
+        chefNuit: kalle,
+        agentJour: bonnet,
+        agentNuit: jean
+      },
+      {
+        jour: 23,
+        chefJour: coulibaly,
+        chefNuit: kalle,
+        agentJour: bonnet,
+        agentNuit: jean
+      },
+      {
+        jour: 24,
+        chefJour: chaouti,
+        chefNuit: kalle,
+        agentJour: disci,
+        agentNuit: koite
+      },
+      {
+        jour: 25,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: koite
+      },
+      {
+        jour: 26,
+        chefJour: lhomme,
+        chefNuit: riquet,
+        agentJour: disci,
+        agentNuit: jean
+      },
+      {
+        jour: 27,
+        chefJour: lhomme,
+        chefNuit: kalle,
+        agentJour: ketir,
+        agentNuit: jean
+      },
+      {
+        jour: 28,
+        chefJour: lhomme,
+        chefNuit: kalle,
+        agentJour: ketir,
+        agentNuit: jean
+      },
+      {
+        jour: 29,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: frahi,
+        agentNuit: koite
+      },
+      {
+        jour: 30,
+        chefJour: chaouti,
+        chefNuit: riquet,
+        agentJour: frahi,
+        agentNuit: koite
+      },
+      {
+        jour: 31,
+        chefJour: vincent,
+        chefNuit: kalle,
+        agentJour: ketir,
+        agentNuit: jean
+      }
+    ]
+    /*{
+      chefs: [
+        {
+          lastname: 'chaouti',
+          firstname: 'brahim',
+          jour: [15, 16, 19, 20, 21, 24, 25, 29, 30]
+        },
+        {
+          lastname: 'COULIBALY',
+          firstname: 'OUMAR',
+          jour: [1, 2, 17, 18, 22, 23]
+        },
+        {
+          lastname: 'DOSSOU',
+          firstname: 'VICTORIN',
+          nuit: [2, 3, 7, 8, 12, 13, 14, 17]
+        },
+        {
+          lastname: 'kalle',
+          firstname: 'oumar',
+          nuit: [22, 23, 24, 27, 28, 31]
+        },
+
+        {
+          lastname: 'lhomme',
+          firstname: 'pascal',
+          jour: [4, 5, 8, 9, 10, 11, 12, 26, 27, 28, 31]
+        },
+        {
+          lastname: 'renaud',
+          firstname: 'cyril',
+          jour: [3, 6, 7, 13, 14]
+        },
+        {
+          lastname: 'riquet',
+          firstname: 'fabrice',
+          nuit: [1, 4, 5, 6, 15, 16, 18, 19, 20, 21, 25, 26, 29, 30]
+        }
+      ],
+      agents: [
+        {
+          lastname: 'Bonnet',
+          firstname: 'Sébastien',
+          jour: ['3', '4', '8', '9', '22', '23']
+        },
+        {
+          lastname: 'Disci',
+          firstname: 'Alex',
+          jour: ['1', '2', '10', '11', '15', '16', '24', '25', '26']
+        },
+        {
+          lastname: 'Frahi',
+          firstname: 'Djamal',
+          jour: [12, 13, 14, 19, 20, 21, 29, 30]
+        },
+        {
+          lastname: 'Willy',
+          firstname: 'Jean',
+          nuit: [3, 4, 9, 10, 13, 14, 17, 18, 22, 23, 26, 27, 28]
+        },
+
+        {
+          lastname: 'Ketir',
+          firstname: 'Djamel',
+          jour: [17, 18, 27, 28, 31]
+        },
+        {
+          lastname: 'Khan',
+          firstname: 'Usman',
+          nuit: [19]
+        },
+        {
+          lastname: 'Koite',
+          firstname: 'Cueikh Makhfou',
+          nuit: [7, 8, 15, 16, 20, 21, 24, 25, 29, 30]
+        },
+        {
+          lastname: 'Kone',
+          firstname: 'Oumar',
+          jour: [5, 6, 7]
+        },
+        {
+          lastname: 'kUHOSA GIGI',
+          firstname: 'Germain',
+          nuit: [1, 2, 5, 6, 11, 12]
+        }
+      ]
+    }
+    */
+  }
+}
+
+export default plannings
